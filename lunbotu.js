58ca53e3c0c2af2943a59d8cec21b7e8d6125248
15364962298 <1105986345@qq.com>
2018-08-24 11:08:36 +0800

    1

--- lunbotu.js
+++ lunbotu.js
@@ -3,46 +3,90 @@ window.onload=function(){
     let point=document.querySelectorAll('.lunbodian li')
     let btnleft=document.querySelector('.btnleft')
     let btnright=document.querySelector('.btnright')
-    console.log(btnleft,btnright)
-    let t=setInterval(fn,1500);
+    let banner=document.querySelector('.banner')
+    let width=parseInt((getComputedStyle(banner,null).width))
+    console.log(lis,banner,width)
     let now=next=0;
+    let flag=true;
+    let t=setInterval(fn,1500);
     function fn(){
         next++;
         if(next==lis.length){
             next=0;
         }
-
-        lis[next].style.left="1226px";
-        animate(lis[now],{left:-1226});
-        animate(lis[next],{left:0,zIndex:10});
-        point[now].style.background=''
-        point[next].style.background='#fff'
-
+        lis[next].style.left=width+"px";
+        animate(lis[now],{left:-width});
+        animate(lis[next],{left:0},function(){
+            flag=true;
+        });
+        point[now].classList.remove('quan')
+        point[next].classList.add('quan')
         now=next;
-    }
-    btnright.onclick=function(){
-        fn()
-    }
-    btnleft.onclick=function(){
-        fn1()
     }
     function fn1(){
         next--;
         if(next<0){
             next=lis.length-1;
         }
-        lis[next].style.left="1226px";
-        animate(lis[now],{left:-1226});
-        animate(lis[next],{left:0,zIndex:10});
-        point[now].style.background=''
-        point[next].style.background='#fff'
+        lis[next].style.left=-width+"px";
+        animate(lis[now],{left:width});
+        animate(lis[next],{left:0},function(){
+            flag=true;
+        });
+        point[now].classList.remove('quan')
+        point[next].classList.add('quan')
         now=next;
     }
-    for(let i=0;i<lis.length;i++){
-        lis[i].onmuseenter=function(){
-            clearInterval(t);
-        }
+    banner.onmouseenter=function(){
+        clearInterval(t)
     }
+    banner.onmouseleave=function(){
+        t=setInterval(fn,1000);
+    }
+    btnright.onclick=function(){
+        if(flag==false){
+            return;
+        }
+        flag=false;
+        fn()
+    }
+    btnleft.onclick=function(){
+        if(flag==false){
+            return;
+        }
+        flag=false;
+        fn1()
+    }
+    point.onmouseenter=function(){
+        clearInterval(t)
+    }
+    point.onmouseleave=function(){
+        t=setInterval(fn,1000);
+    }
+
+
+    point.forEach(function(value,i){
+        value.onclick=function(){
+            if(now==i){
+                return;
+            }
+            else if(now<i){
+                lis[i].style.left=width+"px";
+                animate(lis[now],{left:-width});
+                animate(lis[i],{left:0});
+                point[now].classList.remove('quan')
+                point[i].classList.add('quan')
+            }
+            else{
+                lis[i].style.left=-width+"px";
+                animate(lis[now],{left:width});
+                animate(lis[i],{left:0});
+                point[now].classList.remove('quan')
+                point[i].classList.add('quan')
+            }
+            now=i
+        }
+    })
 
 
 
